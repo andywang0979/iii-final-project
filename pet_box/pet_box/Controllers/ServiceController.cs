5720996b34952a8f9960d09b56537904dd5283a1
Philip9999 <42296254+Philip9999@users.noreply.github.com>
2018-08-30 15:01:59 +0800

    Add SaleController Add ServiceController

---
+++ pet_box/pet_box/Controllers/ServiceController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pet_box.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: Service
+
+        public ActionResult Management()
+        {
+            return View();
+        }
+
+        public ActionResult Workdistinction()
+        {
+            return View();
+        }
+
+        public ActionResult Customerservice()
+        {
+            return View();
+        }
+
+        public ActionResult Customerservice2()
+        {
+            return View();
+        }
+
+
+
+    }
+}
