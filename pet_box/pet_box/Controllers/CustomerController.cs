0c1e9877b03b25b704f2d015121ced78e279359a
Philip9999 <42296254+Philip9999@users.noreply.github.com>
2018-08-31 17:06:04 +0800

    Add CustomerController Add jpg Add View Customer Add Layout

---
+++ pet_box/pet_box/Controllers/CustomerController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pet_box.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult Index3()
+        {
+            return View();
+        }
+
+        //SignOut
+        public ActionResult SignOut()
+        {
+            return View();
+        }
+
+        //Signout
+        public ActionResult SignOut1()
+        {
+            return View();
+        }
+
+        //Signout
+        public ActionResult SignOut2()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult OrderCompleted()
+        {
+            return View();
+        }
+
+    }
+}
