7521a197eb34e5a6069a947899c580d8502dd4e6
thegreathank <38274754+thegreathank@users.noreply.github.com>
2018-09-04 15:45:28 +0800

    create optional item page

---
+++ pet_box/pet_box/Controllers/OptionalItemController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using pet_box.Models;
+using pet_box.ViewModels;
+
+
+namespace pet_box.Controllers
+{
+    public class OptionalItemController : Controller
+    {
+        PetBoxEntities3 db = new PetBoxEntities3();
+        // GET: OptionalItem
+        public ActionResult Index()
+        {
+            OptionalItemViewModel viewM = new OptionalItemViewModel();
+
+            // create a diction to contain the variable name and the object
+            Dictionary<string, CategoryProductModel> dummy = new Dictionary<string, CategoryProductModel>();
+            viewM.CategoryProductModelDic = dummy;
+            
+            int? CategoryIdMax = db.Categories.Max(u => (int?)u.CategoryID);
+            for (int i = 2; i <= CategoryIdMax; i++) {
+                // categoryID with 2 digit
+                string s = String.Format("categoryId{0}", i.ToString("D2"));
+
+                // select coresponding collections of products, and tolist
+                var queryProductDynamic = (from o in db.Products
+                                           where o.ProductID > 1 && o.CategoryID == i
+                                           select o).ToList();
+               
+                CategoryProductModel tempObj = new CategoryProductModel();
+
+                tempObj.CategoryID = queryProductDynamic[0].CategoryID;
+                tempObj.CategoryName = queryProductDynamic[0].Category.CategoryName;
+                tempObj.Products = queryProductDynamic;
+
+                viewM.CategoryProductModelDic[s] = tempObj;
+            }
+
+            return View(viewM);
+        }
+    }
+}
---
+++ pet_box/pet_box/Models/CategoryProductModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pet_box.Models {
+    public class CategoryProductModel {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
